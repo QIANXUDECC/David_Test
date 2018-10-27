570542464b68fc6361d5398a2c7600b5f7acf84b
David Chen <cc291009015@163.com>
2018-10-27 09:04:40 +0800

    Change the "button5"

--- app/src/main/java/com/example/lenovo/myapplication1_3/FirstActivity.java
+++ app/src/main/java/com/example/lenovo/myapplication1_3/FirstActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -18,5 +19,11 @@ public class FirstActivity extends AppCompatActivity {
         Toast.makeText(this,data.toString(),Toast.LENGTH_SHORT).show();
 
         Button button5=(Button)findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
     }
 }
